5c75ffb72e7a477b1d5468b51ce813c816e25f76
xdf <88268897+xdf22@users.noreply.github.com>
2025-08-03 18:49:12 +0300

    change version string to 2.2.25

--- src/doomdef.h
+++ src/doomdef.h
@@ -151,8 +151,8 @@ extern FILE *logstream;
 #else
 #define VERSION    201 // Game version
 #define SUBVERSION 25  // more precise version number
-#define VERSIONSTRING "v2.1.25"
-#define VERSIONSTRINGW L"v2.1.25"
+#define VERSIONSTRING "v2.2.25"
+#define VERSIONSTRINGW L"v2.2.25"
 // Hey! If you change this, add 1 to the MODVERSION below!
 // Otherwise we can't force updates!
 #endif
